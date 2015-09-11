2c8661b7aa7fa34b6ed2c62b2924f9b62d0f02cb
Kentryo <kenhuang@udel.edu>
2015-09-10 20:32:47 -0400

    make plot 1

--- plot1.R
+++ plot1.R
@@ -2,5 +2,15 @@
 rawdata <- read.csv("./household_power_consumption.txt", header = TRUE, sep = ";", stringsAsFactors = FALSE)
 
 #convert date and subset data
-rawdata$Date <- as.Date(rawdata$Date, format = "%d/%m/%y")
+rawdata$Date <- as.Date(rawdata$Date, format = "%d/%m/%Y")
 data_subset <- subset(rawdata, subset = (Date >= "2007-02-01" & Date <= "2007-02-02"))
+
+#convert target to numeric
+data_subset$Global_active_power <- as.numeric(data_subset$Global_active_power)
+
+#make histgram
+hist(data_subset$Global_active_power, col = "red", main="Global Active Power", xlab="Global Active Power (kilowatts)", ylab = "Frequency")
+
+#save to png
+dev.copy(png, file="plot1.png", height=480, width=480)
+dev.off()
